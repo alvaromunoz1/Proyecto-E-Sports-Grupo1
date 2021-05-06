106e7e6ce0234494117e0047ae320583a21bcb25
imanol <imanol@ikasle.egibide.org>
2021-05-06 11:53:17 +0200

    TRIGGER-3.1

--- TRIGGER-BLOCK-CALENDAR.sql
+++ TRIGGER-BLOCK-CALENDAR.sql
@@ -1,26 +1,22 @@
-CREATE OR REPLACE TRIGGER Block_Calendario
-BEFORE INSERT OR UPDATE OR DELETE on Equipo
+CREATE OR REPLACE TRIGGER Block_Cal
+BEFORE INSERT OR UPDATE ON Equipo
 DECLARE
     cal_nombre VARCHAR2(50);
-    eki_nombre VARCHAR2(50);
-BEGIN 
-    SELECT Nombre
-    INTO cal_nombre
+    cal_nom_eki VARCHAR2(50);
+    RAISE_APLICATION_ERROR
+BEGIN
+    SELECT Nombre 
+    INTO cal_nombre 
     FROM Calendario;
 
-    SELECT Nombre
-    INTO eki_nombre
+    SELECT DISTINCT(Nombre_Calendario)
+    INTO cal_nom_eki
     FROM Equipo
     WHERE Nombre_Calendario=cal_nombre;
-    
-    IF (eki_nombre) THEN
-      raise_application_error(-20500,'No se pueden realizar modificaciones con
-      el calendario cerrado');
+
+    IF (cal_nom_eki)=(cal_nombre) THEN
+    RAISE_APLICATION_ERROR(-20001, 'No se pueden modificar los equipos con el 
+    calendario cerrado.');
     END IF;
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-      NULL;
-    WHEN too_many_rows then
-       raise_application_error (-20500,'');
-  END ;
-  
+
+END Block_Cal;
