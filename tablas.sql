93b51d4ede17aeca7d6f7c9f0923fb5938973a64
SergioOlabuenaga <sergio.olabuenaga@ikasle.egibide.org>
2021-05-06 14:08:14 +0200

    tablas

--- tablas.sql
+++ tablas.sql
@@ -2,70 +2,75 @@ drop table Calendario cascade constraints;
 drop table Jornada cascade constraints;
 drop table Partido cascade constraints;
 drop table Equipo cascade constraints;
-drop table Juega cascade constraints;
+drop table Personas cascade constraints;
 drop table Dueino cascade constraints;
 drop table Entrenador cascade constraints;
+drop table Asistente cascade constraints;
 drop table Jugador cascade constraints;
 drop table Usuarios cascade constraints;
 
 create table Calendario(
-Nombre  varchar2(50) PRIMARY KEY
+ID      INT PRIMARY KEY,
+Nombre  varchar2(50)
 );
 
 create table Jornada(
-Nombre              varchar2(50) PRIMARY KEY,
+ID                  INT PRIMARY KEY,
+Nombre              varchar2(50),
 Fecha               date,
-Nombre_Calendario   VARCHAR2(50),
-CONSTRAINT Nombre_Cal_FK foreign key (Nombre_Calendario)
-references Calendario (Nombre)
+ID_Calendario       INT,
+CONSTRAINT ID_Cal_FK foreign key (ID_Calendario)
+references Calendario (ID)
 );
 
 create table Partido(
-Nombre              varchar2(50) PRIMARY KEY,
+ID                  INT PRIMARY KEY,
+Nombre              varchar2(50),
 Hora                TIMESTAMP,
-Nombre_Jornada      VARCHAR2(50),
-CONSTRAINT Nombre_Jornada_FK foreign key (Nombre_Jornada) 
-references Jornada (Nombre)
+ID_Jornada          INT,
+CONSTRAINT ID_Jornada_FK foreign key (ID_Jornada) 
+references Jornada (ID)
 );
 
 create table Equipo(
-Nombre                  varchar2(50) PRIMARY KEY,
-Nombre_Calendario       VARCHAR2(50),
-CONSTRAINT Nombre_Calendario_FK foreign key (Nombre_Calendario)
-references Calendario (Nombre)
+ID                      INT PRIMARY KEY,
+Nombre                  varchar2(50),
+ID_Calendario           INT,
+CONSTRAINT ID_Calendario_FK foreign key (ID_Calendario)
+references Calendario (ID)
 );
 
-create table Juega(
-ID                  INT PRIMARY KEY,
-Nombre_Partido       VARCHAR2(50),
-Nombre_Equipo1       VARCHAR2(50),
-Nombre_Equipo2       VARCHAR2(50),
-CONSTRAINT Nombre_Partido_FK foreign key (Nombre_Partido)
-references Partido (Nombre),
-CONSTRAINT Nombre_Equipo1_FK foreign key (Nombre_Equipo1)
-references Equipo (Nombre),
-CONSTRAINT Nombre_Equipo2_FK foreign key (Nombre_Equipo2)
-references Equipo (Nombre)
-);
-
-create table Dueino(
+create table Pesonas(
 ID                  INT PRIMARY KEY,
 Nombre              varchar2(20),
 Nick                VARCHAR2(15),
-Sueldo              INT,
-Nombre_Equipo       VARCHAR2(50),
-CONSTRAINT Nombre_Equipo_FK foreign key (Nombre_Equipo)
-references Equipo (Nombre)
+Sueldo              NUMBER(5,2),
+ID_Equipo           INT,
+CONSTRAINT ID_Equipo_FK foreign key (ID_Equipo)
+references Equipo (ID)
 );
+
+create table Dueino(
+Anios_Dueino        INT
+) inherits (Personas);
 
 create table Entrenador(
 ID                  INT PRIMARY KEY,
 Nombre              varchar2(20),
 Nick                VARCHAR2(15),
-Sueldo              INT,
+Sueldo              NUMBER(5,2),
 Nombre_Equipo       VARCHAR2(50),
-Asistente           varchar2(20) null,
 CONSTRAINT Nombre_Equipo3_FK foreign key (Nombre_Equipo)
+references Equipo (Nombre)
+);
+
+create table Asistente(
+ID                  INT PRIMARY KEY,
+Nombre              varchar2(20),
+Nick                VARCHAR2(15),
+Sueldo              NUMBER(5,2),
+Nombre_Equipo       VARCHAR2(50),
+CONSTRAINT Nombre_Equipo6_FK foreign key (Nombre_Equipo)
 references Equipo (Nombre)
 );
 
@@ -73,7 +78,7 @@ create table Jugador(
 ID                  INT PRIMARY KEY,
 Nombre              varchar2(20),
 Nick                VARCHAR2(15),
-Sueldo              INT,
+Sueldo              NUMBER(5,2),
 Rol                 varchar2(10),
 Nombre_Equipo       VARCHAR2(50),
 CONSTRAINT Nombre_Equipo4_FK foreign key (Nombre_Equipo)
